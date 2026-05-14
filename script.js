3e9937747a64e5568aeb5de7dec60357ad7c5305
ayran-alves <ayrancruzalves8@gmail.com>
2026-05-14 16:19:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,13 @@
-//Script
-let texto = document.getElementById("texto")
+//server.js
+//npm init -y => INICIA O PROJETO DO node
+//npm install express
 
-function mudar() {
-    texto.innerHTML = "Trocar o texto"
-}
-
-//------------------
-
-let texto2 = document.getElementById("texto2")
-let botao = document.getElementById("botao")
-
-botao.addEventListener("click", function () {
-    texto2.innerHTML = "Mudou com o evento"
+const express = require("express")
+const app = express()
+//localhost: 3000
+app.get("/", function(req, res) {
+    res.send("servidor Funcionando")
 })
-
-//------------------
-
-let input = document.getElementById("input")
-let mensagem = document.getElementById("mensagem")
-input.addEventListener("input", function () {
-    mensagem.innerText = input.value
-    //.VALUE PODE FICAR EM QUALQUER LUGAR
+app.listen(3000, function(){
+    console.log("servidor rodando na porta 3000")
 })
-
-//------------------
-
-let clique = document.getElementById("clique")
-let total = document.getElementById("total")
-let contador = 0
-clique.addEventListener("click", function () {
-    alert("Clicou no botão")
-})
-
-
-//-------------------
-
-let texto3 = document.getElementById("texto3")
-texto3.addEventListener("mouseover", function() {
-if(texto3.innerText == "Você achou o texto secreto") {
-    texto3.innerText = "texto incial"
-} else {
-    texto3.innerText =  "Você achou o texto secreto"
-}
-})
-//------------------------------------------
-
-let imagem = document.getElementById("imagem")
-imagem.addEventListener("mouseover", function() {
-    imagem.src = "img/caixa2.png"
-})
-
-imagem.addEventListener("mouseout", function() {
-imagem.src =  "img/caixa1.png"
-})
-
-
-//-----------------------------------
